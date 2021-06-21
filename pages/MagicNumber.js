33eb1a3ee4f95aefb1db530e599e22e4b988e24e
Keita Amadou <amadou.keita@etudiant.univ-reims.fr>
2021-06-21 11:11:48 +0200

    création de randomNimber

---
+++ pages/MagicNumber.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class MagicNumber extends React.Component {
+    randomNumber = () =>{
+        //faire apparaitre un nombre random
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>MagicNumber = </Text>
+            </View>
+        );
+    }
+}
